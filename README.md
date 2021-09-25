a265cfefdbc97c65bd934fabdadf1ce2ea96106b
David Pacassi Torrico <david@eps-labs.com>
2021-09-25 14:43:50 +0200

    Add a loop for crawling images

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Installation
 See https://www.twilio.com/blog/web-scraping-and-parsing-html-in-python-with-beautiful-soup
-`pip install requests==2.22.0 beautifulsoup4==4.8.1`
+`pip3 install requests==2.22.0 beautifulsoup4==4.8.1`
--- crawler.py
+++ crawler.py
@@ -3,18 +3,24 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-detail_url = 'https://customprints.nga.gov/detail/461264/'
-html_text = requests.get(detail_url).text
-soup = BeautifulSoup(html_text, 'html.parser')
-image_tag = soup.find('link', rel='image_src')
+start_id = 461264
+download_count = 1024
 
-if image_tag is not None:
-    image_url = image_tag['href']
+for i in range(start_id, start_id + download_count):
+    detail_url = 'https://customprints.nga.gov/detail/' + str(i)
+    html_text = requests.get(detail_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    image_tag = soup.find('link', rel='image_src')
 
-    if image_url is not None:
-        url_details = urlparse(image_url)
-        url_filename = os.path.basename(url_details.path)
-        response = requests.get(image_url)
-        file = open('data/' + url_filename, 'wb')
-        file.write(response.content)
-        file.close()
+    if image_tag is not None:
+        image_url = image_tag['href']
+
+        if image_url is not None:
+            url_details = urlparse(image_url)
+            url_filename = os.path.basename(url_details.path)
+            response = requests.get(image_url)
+            file = open('data/' + url_filename, 'wb')
+            file.write(response.content)
+            file.close()
+
+print('Downloaded ' + download_count + ' images')
